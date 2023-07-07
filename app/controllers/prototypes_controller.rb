327a17828f433e528476dbc20a6a32313c6e494f
AK39158 <kei199701@gmail.com>
2023-07-07 15:26:21 +0900

    実装課題　２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,19 @@
 class PrototypesController < ApplicationController
   def index
+    
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+ 
 end
