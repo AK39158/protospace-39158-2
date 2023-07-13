463d7ff1d7e96a84d2e4a89a608b9a3e9f97ffb0
AK39158 <kei199701@gmail.com>
2023-07-13 09:41:49 +0900

    実装課題　３完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
+
+  
   def index
-    
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -15,5 +17,38 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
- 
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
+
